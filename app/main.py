d36432ac4d4757ad970df505de9faa35d08b907a
Muhib Hasan <hmuhib7@gmail.com>
2026-07-10 23:29:21 +1000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,30 +1,31 @@
-import sys
 import os
+import sys
+import shutil
 
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
 
-    acceptable_paths = os.environ.get("PATH", "").split(os.pathsep)
-    
+    builtin_commands = ["exit", "echo", "type"]
+
     while True:
         sys.stdout.write("$ ")
         command = input()
-        if command == "exit":
+        input_commands = command.strip().split()
+
+        if input_commands[0] == "exit":
             break
-        elif command.startswith("echo "):
-            print(command[5:])
-        elif command.startswith("type "):
-            if command[5:] in ["exit", "echo", "type"]:
-                print(f"{command[5:]} is a shell builtin")
+        elif input_commands[0] == "echo":
+            print(" ".join(input_commands[1:]))
+        elif input_commands[0] == "type":
+            if input_commands[1] in builtin_commands:
+                print(f"{input_commands[1]} is a shell builtin")
+            elif shutil.which(input_commands[1]):
+                print(f"{input_commands[1]} is {shutil.which(input_commands[1])}")
             else:
-                match = find_executable(command[5:], acceptable_paths)
-                if match:
-                    print(f"{command[5:]} is {match}")
-                else:
-                    print(f"{command[5:]}: not found")
+                print(f"{input_commands[1]}: not found")
         else:
-            print(f"{command}: command not found")
+            print(f"{input_commands[0]}: command not found")
     
 def find_executable(command, paths):
     for path in paths:
