f345830aa3c4d9a5dc0809b2ea8d4e3dc12706b5
Muhib Hasan <hmuhib7@gmail.com>
2026-07-10 14:45:02 +1000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
-    sys.stdout.flush()
-    command = sys.stdin.readline().strip()
-    sys.stdout.write(f"{command}: command not found\n")
+    
+    command = input()
+    print(f"{command}: command not found")
 
     pass
     
